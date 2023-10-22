5aec92cfa93cadd96cf28de8c336f293de38c654
insorker <2677331029@qq.com>
2023-10-22 21:44:15 +0800

    fix: add img

--- demo.ts
+++ demo.ts
@@ -2,6 +2,6 @@ import { Terminal } from './src/js/components/terminal'
 import img from './img.json'
 
 let content = <HTMLDivElement>document.getElementsByClassName('content')[0]
-let terminal = Terminal.newTerminal()
+let terminal = Terminal.newTerminal(img)
 
 content.appendChild(terminal)
